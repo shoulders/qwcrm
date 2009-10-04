2ac7ebf562db41bd1c6777947d320665608a64df
Glen Vanderhel <myitcrm@gmail.com>
2009-10-04 11:46:20 +0000

    Initial Upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
