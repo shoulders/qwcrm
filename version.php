ed48c71effd66ef1e037219064e3ca7f21743b36
shaunburdick <shaunburdick@gmail.com>
2009-10-18 20:57:34 +0000

    Ticket#0 Desc:Testing SVN access - Changed Version File ToDo:

--- version.php
+++ version.php
@@ -3,5 +3,5 @@
 # Version.php              									#
 # Version 0.2.7.4	TBA             						#
 #############################################################
-@define('MYIT_CRM_VERSION', ' BETA - 0.2.8.0');## See changelog.txt for details.
+@define('MYIT_CRM_VERSION', ' BETA - 0.2.9.0 - SVN');## See changelog.txt for details.
 ?>
