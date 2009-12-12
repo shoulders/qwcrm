ffacb21521efe41b324cc7dc509f34ab5c34d547
Glen Vanderhel <myitcrm@gmail.com>
2009-12-12 05:55:22 +0000

    Ticket #0 - Desc:Changed version and test out google svn - TODO:

--- version.php
+++ version.php
@@ -3,5 +3,5 @@
 # Version.php              									#
 # Version 0.2.7.4	TBA             						#
 #############################################################
-@define('MYIT_CRM_VERSION', ' BETA - 0.2.8.1');## See changelog.txt for details.
+@define('MYIT_CRM_VERSION', ' SVN BETA - 0.2.9.0');## See changelog.txt for details.
 ?>
