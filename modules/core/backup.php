ca94c819a73289ebf139925ebc25daa2d5da5047
Glen Vanderhel <myitcrm@gmail.com>
2009-12-24 03:49:54 +0000

    - Fixes #0 - Desc: Added new sections for upcoming support request submittion forms/generator - TODO: Heaps still

--- modules/core/backup.php
+++ modules/core/backup.php
@@ -1,69 +1,45 @@
 <?php
-/* Load MySQL Database Settings */
-include_once('../../conf.php');
-//$root = getenv("DOCUMENT_ROOT");
-$ip = $_SERVER['REMOTE_ADDR'];
-Print $ip;
+	require_once('Mail.php');
+	require_once('Mail/mime.php');
+        require_once('../../conf.php');
 
-backup_tables($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+	// location of your temp directory
+	$tmpDir = "../../cache/";
+		
+	/* email settings... */
+	$to = "myitcrm@gmail.com";
+	$from = "admin@localhost";
+	$subject = "db - backup";
+	$sqlFile = $tmpDir.PRFX.date('Y_m_d').".sql";
+	$attachment = $tmpDir.PRFX.date('Y_m_d').".tgz";
+        
 
-/* backup the db OR just a table */
-function backup_tables($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $tables = '*')
-{
-	
-	$link = mysql_connect( $DB_HOST, $DB_USER, $DB_PASS );
-	mysql_select_db($DB_NAME, $link );
-	
-	//get all of the tables
-	if($tables == '*')
-	{
-		$tables = array();
-		$result = mysql_query('SHOW TABLES');
-		while($row = mysql_fetch_row($result))
-		{
-			$tables[] = $row[0];
-		}
-	}
-	else
-	{
-		$tables = is_array($tables) ? $tables : explode(',',$tables);
-	}
-	
-	//cycle through
-	foreach($tables as $table)
-	{
-		$result = mysql_query('SELECT * FROM '.$table);
-		$num_fields = mysql_num_fields($result);
-		
-		$return.= 'DROP TABLE '.$table.';';
-		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
-		$return.= "\n\n".$row2[1].";\n\n";
-		
-		for ($i = 0; $i < $num_fields; $i++) 
-		{
-			while($row = mysql_fetch_row($result))
-			{
-				$return.= 'INSERT INTO '.$table.' VALUES(';
-				for($j=0; $j<$num_fields; $j++) 
-				{
-					$row[$j] = addslashes($row[$j]);
-					$row[$j] = ereg_replace("\n","\\n",$row[$j]);
-					if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
-					if ($j<($num_fields-1)) { $return.= ','; }
-				}
-				$return.= ");\n";
-			}
-		}
-		$return.="\n\n\n";
-	}
-	
-	//save file
-	$handle = fopen('../../backup/DB-'.$DB_NAME.'-'.time().'.sql','w+');
-	fwrite($handle,$return);
-	fclose($handle);
-}
-Echo "The database has now been backed up successfully. File is named DB-".$DB_NAME."-".time().".sql."
+        // TODO - Comment out when publishing live F:\M\MyITCRM Dev\xampp\mysql\bin\mysqldump.exe
+        //$sqlFile = $tmpDir.PRFX.date('Y_m_d').".sql";
+        //$path_to_mysqldump = "F:\\M\\MyITCRM_Dev\\xampp\\mysql\\bin" ;
+        //$creatBackup = "F:\\M\\MyITCRM_Dev\\xampp/mysql/bin/mysqldump.exe -u ".DB_USER." --password=".DB_PASS." ".DB_NAME." > ".$sqlFile;
+
+        
+        //The below line is used in LIVE sites - Please uncomment
+        $creatBackup = "mysqldump -u ".DB_USER." --password=".DB_PASS." ".DB_NAME." > ".$sqlFile;
+	$createZip = "tar cvzf $attachment $sqlFile";
+	exec($creatBackup);
+	exec($createZip);
+        
+	$headers = array('From'    => $from, 'Subject' => $subject);
+	$textMessage = $attachment;
+	$htmlMessage = "";
+
+	$mime = new Mail_Mime("\n");
+	$mime->setTxtBody($textMessage);
+	$mime->setHtmlBody($htmlMessage);
+	$mime->addAttachment($attachment, 'text/plain');
+	$body = $mime->get();
+	$hdrs = $mime->headers($headers);
+	$mail = &Mail::factory('mail');
+	$mail->send($to, $hdrs, $body);
+
+	unlink($sqlFile);
+	unlink($attachment);
+        
 ?>
-<iframe src ="../../backup/bigdump.php" width="100%" height="800">
-							<p>Your browser does not support iframes.</p>
-						</iframe>
