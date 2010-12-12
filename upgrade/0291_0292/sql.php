dc2e252a0e29e30d53f20598e70ad789f3951d8e
Glen Vanderhel <myitcrm@gmail.com>
2010-12-12 21:11:06 +0000

    REFS: #156 DESC: Setting up new upgarde sections and files. TODO: Need to add the correct records to add and delete. LINK:

---
+++ upgrade/0291_0292/sql.php
@@ -0,0 +1,33 @@
+<?php
+##################################
+# create_acl								#
+##################################
+if(!create_acl($db) ) {
+echo("<tr>\n
+			<td>UPDATED TABLE ".PRFX."ACL</td>\n
+			<td><font color=\"red\"><b>Failed:</b></font> ". $db->ErrorMsg() ."</td>\n
+		</tr>\n");
+	$error_flag = true;
+} else {
+	echo("<tr>\n
+				<td>UPDATED TABLE ".PRFX."ACL</td>\n
+				<td><font color=\"green\"><b>OK</b></font></td>\n
+			</tr>\n");
+}
+
+function create_acl($db) {
+        
+        $q = "INSERT INTO `".PRFX."ACL` VALUES (77, 'invoice:delete', 1, 1, 1, 1, 0)";
+
+                                $rs = $db->Execute($q);
+                                if(!$rs) {
+                                        return false;
+                                } else {
+                                        return true;
+                                }       
+
+}
+
+
+?>
+
