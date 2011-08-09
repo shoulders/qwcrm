717899c2a491e1c8b0f484257a08c1149d5ddf53
Glen Vanderhel <myitcrm@gmail.com>
2011-08-09 07:09:09 +0000

    Code alignments.Cosmetic

--- modules/invoice/include.php
+++ modules/invoice/include.php
@@ -3,134 +3,136 @@
 # 	Display Open Invoice		#
 #########################################
 
-function display_open_invoice($db,$page_no,$smarty) {
-	
-global $smarty;
-	
-	// Define the number of results per page
-	$max_results = 25;
-	
-	// Figure out the limit for the Execute based
-	// on the current page number.
-	$from = (($page_no * $max_results) - $max_results);  
-	
-	$sql = "SELECT ".PRFX."TABLE_INVOICE.*,
-				".PRFX."TABLE_CUSTOMER. CUSTOMER_DISPLAY_NAME, CUSTOMER_ADDRESS, CUSTOMER_CITY, CUSTOMER_STATE, CUSTOMER_ZIP, CUSTOMER_PHONE, CUSTOMER_WORK_PHONE, CUSTOMER_MOBILE_PHONE, CUSTOMER_EMAIL, CUSTOMER_TYPE, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CREATE_DATE, LAST_ACTIVE ,
-				".PRFX."TABLE_EMPLOYEE.*
-			FROM ".PRFX."TABLE_INVOICE 
-				LEFT JOIN ".PRFX."TABLE_CUSTOMER ON (".PRFX."TABLE_INVOICE.CUSTOMER_ID = ".PRFX."TABLE_CUSTOMER.CUSTOMER_ID)
-				LEFT JOIN ".PRFX."TABLE_EMPLOYEE ON (".PRFX."TABLE_INVOICE.EMPLOYEE_ID = ".PRFX."TABLE_EMPLOYEE.EMPLOYEE_ID)
-			WHERE INVOICE_PAID=".$db->qstr(0)." ORDER BY INVOICE_ID DESC LIMIT $from, $max_results";
+function display_open_invoice($db, $page_no, $smarty)
+{
 
-	if(!$rs = $db->Execute($sql)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;	
-	} else {
-		$invoice_arr = $rs->GetArray();
-	}
-	// Figure out the total number of results in DB: 
-	$q = "SELECT COUNT(*) as Num FROM ".PRFX."TABLE_INVOICE WHERE INVOICE_PAID=".$db->qstr(0);
+    global $smarty;
 
-	if(!$results = $db->Execute($q)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	}
-	
+    // Define the number of results per page
+    $max_results = 25;
 
-	if(!$total_results = $results->FetchRow()) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	} else {
-		$smarty->assign('total_results', $total_results['Num']);
-	}
-		
-	// Figure out the total number of pages. Always round up using ceil()
-	$total_pages = ceil($total_results["Num"] / $max_results); 
-	$smarty->assign('total_pages', $total_pages);
-	
-	// Assign the first page
-	if($page_no > 1) {
-    	$prev = ($page_no - 1);	 
-	} 	
+    // Figure out the limit for the Execute based
+    // on the current page number.
+    $from = (($page_no * $max_results) - $max_results);
 
-	// Build Next Link
-	if($page_no < $total_pages){
-    	$next = ($page_no + 1); 
-	}
+    $sql = "SELECT " . PRFX . "TABLE_INVOICE.*,
+				" . PRFX . "TABLE_CUSTOMER. CUSTOMER_DISPLAY_NAME, CUSTOMER_ADDRESS, CUSTOMER_CITY, CUSTOMER_STATE, CUSTOMER_ZIP, CUSTOMER_PHONE, CUSTOMER_WORK_PHONE, CUSTOMER_MOBILE_PHONE, CUSTOMER_EMAIL, CUSTOMER_TYPE, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CREATE_DATE, LAST_ACTIVE ,
+				" . PRFX . "TABLE_EMPLOYEE.*
+			FROM " . PRFX . "TABLE_INVOICE
+				LEFT JOIN " . PRFX . "TABLE_CUSTOMER ON (" . PRFX . "TABLE_INVOICE.CUSTOMER_ID = " . PRFX . "TABLE_CUSTOMER.CUSTOMER_ID)
+				LEFT JOIN " . PRFX . "TABLE_EMPLOYEE ON (" . PRFX . "TABLE_INVOICE.EMPLOYEE_ID = " . PRFX . "TABLE_EMPLOYEE.EMPLOYEE_ID)
+			WHERE INVOICE_PAID=" . $db->qstr(0) . " ORDER BY INVOICE_ID DESC LIMIT $from, $max_results";
+
+    if (!$rs = $db->Execute($sql)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        $invoice_arr = $rs->GetArray();
+    }
+    // Figure out the total number of results in DB:
+    $q = "SELECT COUNT(*) as Num FROM " . PRFX . "TABLE_INVOICE WHERE INVOICE_PAID=" . $db->qstr(0);
+
+    if (!$results = $db->Execute($q)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    }
 
 
-	$smarty->assign('name', $name);
-	$smarty->assign('page_no', $page_no);
-	$smarty->assign("previous", $prev);	
-	$smarty->assign("next", $next);
-	return $invoice_arr;
+    if (!$total_results = $results->FetchRow()) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        $smarty->assign('total_results', $total_results['Num']);
+    }
+
+    // Figure out the total number of pages. Always round up using ceil()
+    $total_pages = ceil($total_results["Num"] / $max_results);
+    $smarty->assign('total_pages', $total_pages);
+
+    // Assign the first page
+    if ($page_no > 1) {
+        $prev = ($page_no - 1);
+    }
+
+    // Build Next Link
+    if ($page_no < $total_pages) {
+        $next = ($page_no + 1);
+    }
+
+
+    $smarty->assign('name', $name);
+    $smarty->assign('page_no', $page_no);
+    $smarty->assign("previous", $prev);
+    $smarty->assign("next", $next);
+    return $invoice_arr;
 }
 
 ########################################
 # Paid Invoices	                       #
 ########################################
 
-function display_paid_invoice($db,$page_no,$smarty) {
-	
-global $smarty;
-	
-	// Define the number of results per page
-	$max_results = 25;
-	
-	// Figure out the limit for the Execute based
-	// on the current page number.
-	$from = (($page_no * $max_results) - $max_results);  
-	
-	$sql = "SELECT ".PRFX."TABLE_INVOICE.*,
-				".PRFX."TABLE_CUSTOMER. CUSTOMER_DISPLAY_NAME, CUSTOMER_ADDRESS, CUSTOMER_CITY, CUSTOMER_STATE, CUSTOMER_ZIP, CUSTOMER_PHONE, CUSTOMER_WORK_PHONE, CUSTOMER_MOBILE_PHONE, CUSTOMER_EMAIL, CUSTOMER_TYPE, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CREATE_DATE, LAST_ACTIVE ,
-				".PRFX."TABLE_EMPLOYEE.*
-			FROM ".PRFX."TABLE_INVOICE 
-				LEFT JOIN ".PRFX."TABLE_CUSTOMER ON (".PRFX."TABLE_INVOICE.CUSTOMER_ID = ".PRFX."TABLE_CUSTOMER.CUSTOMER_ID)
-				LEFT JOIN ".PRFX."TABLE_EMPLOYEE ON (".PRFX."TABLE_INVOICE.EMPLOYEE_ID = ".PRFX."TABLE_EMPLOYEE.EMPLOYEE_ID)
-			WHERE INVOICE_PAID=".$db->qstr(1)." ORDER BY INVOICE_ID DESC LIMIT $from, $max_results";
+function display_paid_invoice($db, $page_no, $smarty)
+{
 
-	if(!$rs = $db->Execute($sql)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	} else {
-		$invoice_arr = $rs->GetArray();
-	}
-	
-	// Figure out the total number of results in DB: 
-	$q = "SELECT COUNT(*) as Num FROM ".PRFX."TABLE_INVOICE WHERE INVOICE_PAID=".$db->qstr(1);
-	if(!$results = $db->Execute($q)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	}
+    global $smarty;
 
+    // Define the number of results per page
+    $max_results = 25;
 
-	if(!$total_results = $results->FetchRow()) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	} else {
-		$smarty->assign('total_results', $total_results['Num']);
-	}
-		
-	// Figure out the total number of pages. Always round up using ceil()
-	$total_pages = ceil($total_results["Num"] / $max_results); 
-	$smarty->assign('total_pages', $total_pages);
-	
-	// Assign the first page
-	if($page_no > 1) {
-    	$prev = ($page_no - 1);	 
-	} 	
+    // Figure out the limit for the Execute based
+    // on the current page number.
+    $from = (($page_no * $max_results) - $max_results);
 
-	// Build Next Link
-	if($page_no < $total_pages){
-    	$next = ($page_no + 1); 
-	}
+    $sql = "SELECT " . PRFX . "TABLE_INVOICE.*,
+				" . PRFX . "TABLE_CUSTOMER. CUSTOMER_DISPLAY_NAME, CUSTOMER_ADDRESS, CUSTOMER_CITY, CUSTOMER_STATE, CUSTOMER_ZIP, CUSTOMER_PHONE, CUSTOMER_WORK_PHONE, CUSTOMER_MOBILE_PHONE, CUSTOMER_EMAIL, CUSTOMER_TYPE, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CREATE_DATE, LAST_ACTIVE ,
+				" . PRFX . "TABLE_EMPLOYEE.*
+			FROM " . PRFX . "TABLE_INVOICE
+				LEFT JOIN " . PRFX . "TABLE_CUSTOMER ON (" . PRFX . "TABLE_INVOICE.CUSTOMER_ID = " . PRFX . "TABLE_CUSTOMER.CUSTOMER_ID)
+				LEFT JOIN " . PRFX . "TABLE_EMPLOYEE ON (" . PRFX . "TABLE_INVOICE.EMPLOYEE_ID = " . PRFX . "TABLE_EMPLOYEE.EMPLOYEE_ID)
+			WHERE INVOICE_PAID=" . $db->qstr(1) . " ORDER BY INVOICE_ID DESC LIMIT $from, $max_results";
+
+    if (!$rs = $db->Execute($sql)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        $invoice_arr = $rs->GetArray();
+    }
+
+    // Figure out the total number of results in DB:
+    $q = "SELECT COUNT(*) as Num FROM " . PRFX . "TABLE_INVOICE WHERE INVOICE_PAID=" . $db->qstr(1);
+    if (!$results = $db->Execute($q)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    }
 
 
-	$smarty->assign('name', $name);
-	$smarty->assign('page_no', $page_no);
-	$smarty->assign("previous", $prev);	
-	$smarty->assign("next", $next);
-	return $invoice_arr;
+    if (!$total_results = $results->FetchRow()) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        $smarty->assign('total_results', $total_results['Num']);
+    }
+
+    // Figure out the total number of pages. Always round up using ceil()
+    $total_pages = ceil($total_results["Num"] / $max_results);
+    $smarty->assign('total_pages', $total_pages);
+
+    // Assign the first page
+    if ($page_no > 1) {
+        $prev = ($page_no - 1);
+    }
+
+    // Build Next Link
+    if ($page_no < $total_pages) {
+        $next = ($page_no + 1);
+    }
+
+
+    $smarty->assign('name', $name);
+    $smarty->assign('page_no', $page_no);
+    $smarty->assign("previous", $prev);
+    $smarty->assign("next", $next);
+    return $invoice_arr;
 }
 
 ##########################################
@@ -138,35 +140,36 @@ global $smarty;
 # Loads language file up as a php array  #
 ##########################################
 
-function gateway_xml2php($module) {
-	global $smarty;
+function gateway_xml2php($module)
+{
+    global $smarty;
 
-	//$file = FILE_ROOT."language".SEP.$module.SEP.LANG ;
-        $file = FILE_ROOT."language".SEP.LANG ;
+    //$file = FILE_ROOT."language".SEP.$module.SEP.LANG ;
+    $file = FILE_ROOT . "language" . SEP . LANG;
 
-   $xml_parser = xml_parser_create();
-   if (!($fp = fopen($file, 'r'))) {
-       die('unable to open XML');
-   }
-   $contents = fread($fp, filesize($file));
-   fclose($fp);
-   xml_parse_into_struct($xml_parser, $contents, $arr_vals);
-   xml_parser_free($xml_parser);
+    $xml_parser = xml_parser_create();
+    if (!($fp = fopen($file, 'r'))) {
+        die('unable to open XML');
+    }
+    $contents = fread($fp, filesize($file));
+    fclose($fp);
+    xml_parse_into_struct($xml_parser, $contents, $arr_vals);
+    xml_parser_free($xml_parser);
 
-   $xmlarray = array();
+    $xmlarray = array();
 
-  foreach($arr_vals as $things){
-		if($things['tag'] != 'TRANSLATE' && $things['value'] != "" ){
+    foreach ($arr_vals as $things) {
+        if ($things['tag'] != 'TRANSLATE' && $things['value'] != "") {
 
-                    $ttag = strtolower($things['tag']);
-                    $tvalue = $things['value'];
+            $ttag = strtolower($things['tag']);
+            $tvalue = $things['value'];
 
-                    $xmlarray[$ttag]= $tvalue;
+            $xmlarray[$ttag] = $tvalue;
 
-		}
-	}
+        }
+    }
 
-	return $xmlarray;
+    return $xmlarray;
 }
 
 #####################################
@@ -175,31 +178,33 @@ function gateway_xml2php($module) {
 
 $labourID = $VAR['labourID'];
 
-function delete_labour_record($db, $labourID){
-	$sql = "DELETE FROM ".PRFX."TABLE_INVOICE_LABOR WHERE INVOICE_LABOR_ID=".$db->qstr($labourID);
+function delete_labour_record($db, $labourID)
+{
+    $sql = "DELETE FROM " . PRFX . "TABLE_INVOICE_LABOR WHERE INVOICE_LABOR_ID=" . $db->qstr($labourID);
 
-	if(!$rs = $db->Execute($sql)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	} else {
-		return true;
-	}
+    if (!$rs = $db->Execute($sql)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        return true;
+    }
 }
 
 #####################################
 #   Delete Parts Record             #
 #####################################
 
-function delete_parts_record($db, $partsID){
-	$sql = "DELETE FROM ".PRFX."TABLE_INVOICE_PARTS WHERE INVOICE_PARTS_ID=".$db->qstr($partsID);
+function delete_parts_record($db, $partsID)
+{
+    $sql = "DELETE FROM " . PRFX . "TABLE_INVOICE_PARTS WHERE INVOICE_PARTS_ID=" . $db->qstr($partsID);
 
 
-	if(!$rs = $db->Execute($sql)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	} else {
-		return true;
-	}
+    if (!$rs = $db->Execute($sql)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        return true;
+    }
 
 
 }
@@ -208,57 +213,61 @@ function delete_parts_record($db, $partsID){
 #   Delete Invoice                  #
 #####################################
 
-function delete_invoice($db, $invoice_id, $customer_id, $login){
+function delete_invoice($db, $invoice_id, $customer_id, $login)
+{
     //Add comment to Transaction Table for Deletion
-    $q = "INSERT INTO ".PRFX."TABLE_TRANSACTION SET
-			DATE 			= ".$db->qstr(time()).",
+    $q = "INSERT INTO " . PRFX . "TABLE_TRANSACTION SET
+			DATE 			= " . $db->qstr(time()) . ",
 			TYPE 			= '6',
-			INVOICE_ID              = ".$db->qstr($invoice_id).",
+			INVOICE_ID              = " . $db->qstr($invoice_id) . ",
 			WORKORDER_ID            = '0',
-			CUSTOMER_ID             = ".$db->qstr($customer_id).",
-			MEMO 			= 'Invoice Deleted by ".$db->qstr($login).",
+			CUSTOMER_ID             = " . $db->qstr($customer_id) . ",
+			MEMO 			= 'Invoice Deleted by " . $db->qstr($login) . ",
 			AMOUNT			= '0'";
-		if(!$rs = $db->execute($q)) {
-			force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1');
-			exit;
-		}
+    if (!$rs = $db->execute($q)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1');
+        exit;
+    }
     //Actual Deletion Function from Invoice Table
-	$sql = "DELETE FROM ".PRFX."TABLE_INVOICE WHERE INVOICE_ID=".$db->qstr($invoice_id);
+    $sql = "DELETE FROM " . PRFX . "TABLE_INVOICE WHERE INVOICE_ID=" . $db->qstr($invoice_id);
 
-	if(!$rs = $db->Execute($sql)) {
-		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
-		exit;
-	} else {
-		return true;
-	}
+    if (!$rs = $db->Execute($sql)) {
+        force_page('core', 'error&error_msg=MySQL Error: ' . $db->ErrorMsg() . '&menu=1&type=database');
+        exit;
+    } else {
+        return true;
+    }
 
 }
+
 #####################################
 #   Sum Labour Sub Totals           #
 #####################################
 
-function labour_sub_total_sum ($db, $invoiceID){
-        $q = "SELECT SUM(INVOICE_LABOR_SUBTOTAL) AS labour_sub_total_sum FROM ".PRFX."TABLE_INVOICE_LABOR WHERE INVOICE_ID=".$db->qstr($invoiceID);
-        if(!$rs = $db->Execute($q)){
-                echo 'Error: '. $db->ErrorMsg();
-                die;
-        }
-        $labour_sub_total_sum = $rs->fields['labour_sub_total_sum'];
-        return $labour_sub_total_sum;
-      }
+function labour_sub_total_sum($db, $invoiceID)
+{
+    $q = "SELECT SUM(INVOICE_LABOR_SUBTOTAL) AS labour_sub_total_sum FROM " . PRFX . "TABLE_INVOICE_LABOR WHERE INVOICE_ID=" . $db->qstr($invoiceID);
+    if (!$rs = $db->Execute($q)) {
+        echo 'Error: ' . $db->ErrorMsg();
+        die;
+    }
+    $labour_sub_total_sum = $rs->fields['labour_sub_total_sum'];
+    return $labour_sub_total_sum;
+}
 
 #####################################
 #   Sum Parts Sub Total             #
 #####################################
 
-      function parts_sub_total_sum ($db, $invoiceID){
-        $q = "SELECT SUM(INVOICE_PARTS_SUBTOTAL) AS parts_sub_total_sum FROM ".PRFX."TABLE_INVOICE_PARTS WHERE INVOICE_ID=".$db->qstr($invoiceID);
-        if(!$rs = $db->Execute($q)){
-                echo 'Error: '. $db->ErrorMsg();
-                die;
-        }
-        $parts_sub_total_sum = $rs->fields['parts_sub_total_sum'];
-        return $parts_sub_total_sum;
-      }
+function parts_sub_total_sum($db, $invoiceID)
+{
+    $q = "SELECT SUM(INVOICE_PARTS_SUBTOTAL) AS parts_sub_total_sum FROM " . PRFX . "TABLE_INVOICE_PARTS WHERE INVOICE_ID=" . $db->qstr($invoiceID);
+    if (!$rs = $db->Execute($q)) {
+        echo 'Error: ' . $db->ErrorMsg();
+        die;
+    }
+    $parts_sub_total_sum = $rs->fields['parts_sub_total_sum'];
+    return $parts_sub_total_sum;
+}
 
 ?>
