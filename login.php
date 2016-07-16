b30e65d86610c0415bb024bb1d8ab0d4b68aa347
Jon Brown <jon@lancastrian-it.co.uk>
2016-07-16 16:18:10 +0100

    removed tabs

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
 #####################################################
-# This program is distributed under the terms and 	#
-# conditions of the GPL								#
-# login.php											#
-# Version 0.0.1	Fri Sep 30 09:30:10 PDT 2005		#
-#													#
+# This program is distributed under the terms and   #
+# conditions of the GPL                             #
+# login.php                                         #
+# Version 0.0.1    Fri Sep 30 09:30:10 PDT 2005     #
+#                                                   #
 #####################################################
 
 #####################################
-#	Load all Configs				#
+#    Load all Configs               #
 #####################################
 require("conf.php");
 
@@ -21,23 +21,23 @@ if(!$rs = $db->execute($q)) {
 
 $smarty->assign('company_logo',$rs->fields['COMPANY_LOGO']);
 #####################################
-#	Initial Authorization				#
+#    Initial Authorization          #
 #####################################
 
 $smarty->assign('page_title', 'Login');
 
 #####################################
-#	Display Any Errors				#
+#    Display Any Errors             #
 ##################################### 
 
 if(isset($_GET["error_msg"]))
 {
-	$smarty->assign('error_msg', $_GET["error_msg"]);
+    $smarty->assign('error_msg', $_GET["error_msg"]);
 }
 
 
 #####################################
-#	Display the pages				#
+#    Display the pages              #
 #####################################
 /* CRM Version */
 /*$smarty->assign('VERSION', VERSION_NAME);*/
