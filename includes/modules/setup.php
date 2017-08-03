4be549ef18655d93532c570be5e0e739feea98ec
Jon Brown <jon@lancastrian-it.co.uk>
2017-08-03 20:04:16 +0100

    closes #402 - addd acl entries and created a holding module.php

---
+++ includes/modules/setup.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * @package   QWcrm
+ * @author    Jon Brown https://quantumwarp.com/
+ * @copyright Copyright (C) 2016 - 2017 Jon Brown, All rights reserved.
+ * @license   GNU/GPLv3 or later; https://www.gnu.org/licenses/gpl.html
+ */
+
+/*
+ * Mandatory Code - Code that is run upon the file being loaded
+ * Display Functions - Code that is used to primarily display records - linked tables
+ * New/Insert Functions - Creation of new records
+ * Get Functions - Grabs specific records/fields ready for update - no table linking
+ * Update Functions - For updating records/fields
+ * Close Functions - Closing Work Orders code
+ * Delete Functions - Deleting Work Orders
+ * Other Functions - All other functions not covered above
+ */
+
+defined('_QWEXEC') or die;
+
+/** Mandatory Code **/
+
+/** Display Functions **/
+
+/** New/Insert Functions **/
+
+/** Get Functions **/
+
+/** Update Functions **/
+
+/** Close Functions **/
+
+/** Delete Functions **/
+
+/** Other Functions **/
