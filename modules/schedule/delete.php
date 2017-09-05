bb77044ab1bcf9abcd5b79328fbca992c4b5a624
Jon Brown <jon@lancastrian-it.co.uk>
2017-09-05 08:36:05 +0100

    closes #516

--- modules/schedule/delete.php
+++ modules/schedule/delete.php
@@ -19,13 +19,16 @@ if(!check_page_accessed_via_qwcrm()) {
 
 // Check if we have a schedule_id
 if($schedule_id == '') {
-    force_page('core', 'dashboard', 'warning_msg='.gettext("No Schedule ID supplied."));
+    force_page('schedule', 'search', 'warning_msg='.gettext("No Schedule ID supplied."));
     exit;
 }
-  
+
+// get workorder_id
+$workorder_id = get_schedule_details($db, $schedule_id, 'workorder_id');
+
 // Delete the schedule
 delete_schedule($db, $schedule_id);
 
 // load schedule search page
-force_page('core', 'dashboard');
+force_page('workorder', 'details&workorder_id='.$workorder_id);
 exit;
