4182f5cc7c6810c978bf97f2adef76203e29a3a5
Jon Brown <jon@lancastrian-it.co.uk>
2017-10-07 19:02:09 +0100

    ref #669

--- modules/customer/search.php
+++ modules/customer/search.php
@@ -18,6 +18,6 @@ $smarty->assign('customer_types',   get_customer_types($db)                     
 $smarty->assign('search_category',  $VAR['search_category']                                                                                                             );
 $smarty->assign('search_term',      $VAR['search_term']                                                                                                                 );
 $smarty->assign('status',           $VAR['status']                                                                                                                      );
-$smarty->assign('search_result',    display_customers($db, 'customer_id', 'DESC', true, $page_no, '25', $VAR['search_term'], $VAR['search_category'], $VAR['status'])    );
+$smarty->assign('search_result',    display_customers($db, 'customer_id', 'DESC', true, $page_no, '25', $VAR['search_term'], $VAR['search_category'], $VAR['status'])   );
 
 $BuildPage .= $smarty->fetch('customer/search.tpl');
