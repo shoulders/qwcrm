73bf861ceb4a9e29b89bfb4e0773db355bd1cc72
Jon Brown <jon@lancastrian-it.co.uk>
2018-05-07 17:35:17 +0100

    closes #641 - swiftmailer is now loaded by composer

--- src/includes/email.php
+++ src/includes/email.php
@@ -21,8 +21,10 @@
 
 defined('_QWEXEC') or die;
 
-// // Load Dependencies Manually (Swiftmailer)
-//require(LIBRARIES_DIR.'swift/swift_required.php');
+/*
+// Load Dependencies Manually (Swiftmailer)
+require(LIBRARIES_DIR.'swift/swift_required.php')
+ */
 
 /* Other Functions */
 
