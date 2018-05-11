95f84f55baf77ac1d2d4502902c7e50a330776ad
Jon Brown <jon@lancastrian-it.co.uk>
2018-05-11 18:22:39 +0100

    ref #694 - corrected some minor typos

--- src/themes/default/js/editor-config.js
+++ src/themes/default/js/editor-config.js
@@ -5,9 +5,9 @@
  * @license   GNU/GPLv3 or later; https://www.gnu.org/licenses/gpl.html
  */
 
-tinymce.init({
+tinymce.init( {
     
-    selector : "textarea:not(.mceNoEditor)",
+    selector : 'textarea:not(.mceNoEditor)',
     
     // On Submit if TinyMCE Editor is empty and has the class "wysiwyg-checkforcontent" on its placeholder, dont submit the form and put a red border around it
     setup: function(editor) {
@@ -49,4 +49,4 @@ tinymce.init({
         'advlist autolink charmap code contextmenu fullscreen help hr insertdatetime link lists paste preview table textcolor visualchars'
     ]    
 
-});
+} );
