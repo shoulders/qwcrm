adbb656ae14e2a33982578b81c3db088d9e26762
Jon Brown <jon@lancastrian-it.co.uk>
2018-06-16 19:43:57 +0100

    ref #821 - adodb is now solely in the framework

--- src/libraries/qframework/qframework.php
+++ src/libraries/qframework/qframework.php
@@ -484,18 +484,86 @@ class QFactory {
      * @since   11.1
      */
     protected static function createDbo()
-    {
-          
+    {        
+        /* Old version but works well
         $conf = self::getConfig();       
         
         // create adodb database connection
         $db = ADONewConnection('mysqli');
         $db->Connect($conf->get('db_host'), $conf->get('db_user'), $conf->get('db_pass'), $conf->get('db_name'));
                 
-        return $db;        
+        return $db;*/
+        
+        /* New Version */
+
+        $conf = self::getConfig();
+        
+        /* ADODB Options */
+
+        // ADODB_ASSOC_CASE - You can control the associative fetch case for certain drivers which behave differently. - native is default since v2.90
+        //define('ADODB_ASSOC_CASE', 1);  // set what case to use for recordsets where the field name (not table names): 0 = lowercase, 1 = uppercase, 2 = native case
+
+        // $ADODB_FETCH_MODE - This is a global variable that determines how arrays are retrieved by recordsets. 
+        //$ADODB_FETCH_MODE = ADODB_FETCH_NUM; 
+
+        /* -- */
+
+        /*
+        // Set Path for ADODB in the php include path (Not needed because it is loaded by Composer)
+        set_include_path(get_include_path() . PATH_SEPARATOR . LIBRARIES_DIR.'adodb/');
+
+        // Load Dependency Manually (Not needed because it is loaded by composer)
+        require('adodb.inc.php');
+
+        // Enable error trapping - this extends the system class Exception - http://adodb.org/dokuwiki/doku.php?id=v5:userguide:error_handling
+        // I think this tries to convert standard PHP errors to Exceptions - needed for get_qwcrm_database_version_number()
+        require('adodb-exceptions.inc.php');
+        require(VENDOR_DIR.'adodb/adodb-php/adodb-exceptions.inc.php');
+        */
+        
+        $db = ADONewConnection('mysqli');
+        
+        // This is needed to allow install/migration/upgrade
+        if($conf->get('db_host') && $conf->get('db_user') || $conf->get('db_name') && $conf->get('db_name')) {
+
+            // Get current PHP error reporting level
+            $reporting_level = error_reporting();
+
+            // Disable PHP error reporting (works globally)
+            error_reporting(0);
+
+            // Create ADOdb database connection - and collection exceptions
+            try
+            {        
+                $db->Connect($conf->get('db_host'), $conf->get('db_user'), $conf->get('db_pass'), $conf->get('db_name'));
+            }
+
+            catch (Exception $e)
+            {
+                // Re-Enable PHP error reporting
+                error_reporting($reporting_level);
+
+                //echo $e->msg;
+                //var_dump($e);
+                //adodb_backtrace($e->gettrace());        
+                //$smarty->assign('warning_msg', $e->msg);
+                
+                return false;
+            }
+
+            // Re-Enable PHP error reporting
+            error_reporting($reporting_level);
+
+            /* Return the connection status
+            if(!$db->isConnected()) {        
+                die('<div style="color: red;">'._gettext("There is a database connection issue. Check your settings in the config file.").'<br><br>'.$db->ErrorMsg().'</div>');      
+            }*/
+
+        } 
+        
+        return $db;
     
     }
-
 
 /****************** Client and Site checks ******************/
     
