a8a431dca9e9482b66b3629279c741e22b96e209
Jon Brown <jon@lancastrian-it.co.uk>
2019-05-12 19:06:30 +0100

    closes #1459

--- src/components/_includes/company.php
+++ src/components/_includes/company.php
@@ -122,10 +122,11 @@ function get_default_vat_tax_code($tax_system = null) {
     if(!$tax_system) {$tax_system = QW_TAX_SYSTEM;}
     
     if($tax_system == 'none') { return 'TNA'; }
-    if($tax_system == 'vat_standard') { return 'T1'; }
-    if($tax_system == 'vat_flat') { return 'T1'; }
+    if($tax_system == 'sales_tax_cash') { return 'TNA'; }     
+    if($tax_system == 'vat_standard') { return 'T1'; }    
     if($tax_system == 'vat_cash') { return 'T1'; }
-    if($tax_system == 'sales_tax') { return 'TNA'; }    
+    if($tax_system == 'vat_flat_standard') { return 'T1'; }
+    if($tax_system == 'vat_flat_cash') { return 'T1'; }       
     
 }
 
@@ -139,18 +140,23 @@ function get_voucher_vat_tax_code($type, $tax_system = null) {
     
     if($type == 'multi_purpose') {
         if($tax_system == 'none') { return 'TNA'; }
-        if($tax_system == 'vat_standard') { return 'T0'; }
-        if($tax_system == 'vat_flat') { return 'T0'; }
+        if($tax_system == 'sales_tax_cash') { return 'TNA'; } 
+        if($tax_system == 'vat_standard') { return 'T0'; }        
         if($tax_system == 'vat_cash') { return 'T0'; }
-        if($tax_system == 'sales_tax') { return 'TNA'; } 
+        if($tax_system == 'vat_flat_standard') { return 'T0'; }
+        if($tax_system == 'vat_flat_cash') { return 'T0'; }
+        
     }
     
     if($type == 'single_purpose') {
         if($tax_system == 'none') { return 'TNA'; }
+        if($tax_system == 'sales_tax_cash') { return 'TNA'; }
         if($tax_system == 'vat_standard') { return 'T1'; }
-        if($tax_system == 'vat_flat') { return 'T1'; }
         if($tax_system == 'vat_cash') { return 'T1'; }
-        if($tax_system == 'sales_tax') { return 'TNA'; } 
+        if($tax_system == 'vat_flat_standard') { return 'T1'; }
+        if($tax_system == 'vat_flat_cash') { return 'T1'; }
+        
+         
     }    
     
 }
