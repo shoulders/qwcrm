a1e656e0ea33b19bf5be4510de2f697395dbe5b2
Jon Brown <jon@lancastrian-it.co.uk>
2020-02-08 17:30:35 +0000

    closes #1717

--- src/setup/upgrade/3_1_3/upgrade_database.sql
+++ src/setup/upgrade/3_1_3/upgrade_database.sql
@@ -4,3 +4,23 @@
  * @copyright Copyright (C) 2016 - 2017 Jon Brown, All rights reserved.
  * @license   GNU/GPLv3 or later; https://www.gnu.org/licenses/gpl.html
  */
+
+--
+-- Correct Payment Status on some records, issue introduced in an earlier version and fixed but records were never corrected
+--
+
+UPDATE `#__invoice_records`
+SET `status` = 'paid', `closed_on` = `last_active`, `is_closed` = 1
+WHERE `status` = 'partially_paid' AND `balance` = 0 AND `unit_gross` > 0;
+
+UPDATE `#__refund_records`
+SET `status` = 'paid', `closed_on` = `last_active`
+WHERE `status` = 'partially_paid' AND `balance` = 0 AND `unit_gross` > 0;
+
+UPDATE `#__expense_records`
+SET `status` = 'paid', `closed_on` = `last_active`
+WHERE `status` = 'partially_paid' AND `balance` = 0 AND `unit_gross` > 0;
+
+UPDATE `#__other_records`
+SET `status` = 'paid', `closed_on` = `last_active`
+WHERE `status` = 'partially_paid' AND `balance` = 0 AND `unit_gross` > 0;
