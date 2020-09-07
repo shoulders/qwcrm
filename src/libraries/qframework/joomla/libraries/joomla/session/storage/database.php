47d8c9dcf4ffbf2d4a46c36ba3eadf0bf38d9d97
Jon Brown <jon@lancastrian-it.co.uk>
2020-09-07 10:17:54 +0100

    Revert "closes #1792 - session Exception fixed"
    This reverts commit 292c56ddffab6f201e5c553eb4e1c2dd29b7e2d9.

--- src/libraries/qframework/joomla/libraries/joomla/session/storage/database.php
+++ src/libraries/qframework/joomla/libraries/joomla/session/storage/database.php
@@ -44,16 +44,13 @@ class JSessionStorageDatabase extends JSessionStorage
             $db->setQuery($query);
 
             $result = (string) $db->loadResult();
-
-            $result = str_replace('\0\0\0', chr(0) . '*' . chr(0), $result)
-
-            return $result;
              */
 
             $sql = "SELECT data FROM ".PRFX."session WHERE session_id = ".$db->qstr($id);
-            $rs = $db->execute($sql);            
-            $result = (string) $rs->fields['data'] ?? false;
-            $result = $result ? str_replace('\0\0\0', chr(0) . '*' . chr(0), $result) : false;
+            $rs = $db->execute($sql);
+            $result = (string) $rs->fields['data'];
+
+            $result = str_replace('\0\0\0', chr(0) . '*' . chr(0), $result);
 
             return $result;
         }
